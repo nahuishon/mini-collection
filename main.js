a9a00e55be26a882c1651bb6d54e0bfbee7c5b99
nahuishon <shonn620@newschool.edu>
2021-03-26 20:30:21 +0900

    D

--- main.js
+++ main.js
@@ -1,65 +1,45 @@
-console.log("My Favorite Korean Movie Collection!");
+let myText = "hello world"; //strings
 
-var Airtable = require("airtable");
-console.log(Airtable); 
+document.getElementById("demo").innerHTML = "My Favorite Korean Movie";
 
+//FIRST IMFORMATION
 
+document.getElementById("button").addEventListener("click", function() {
+    document.getElementById("detail").innerHTML = "PARASITE / BONG JOON-HO";
+  })
 
+  //SECOND IMFORMATION
 
-const base = require('airtable').base('appFM7dyonhVM0A5U');
+  //LAST IMFORMATION
 
-var Airtable = require('airtable');
-Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: 'keyAPSgVKXfGcLldOY'
-});
-var base = Airtable.base('appFM7dyonhVM0A5U');
+  
+//new image
+  let newch = document.querySelector(".newch");
 
+  //the word halfway
+  let increaseNewchImage = document.querySelector('.new-poster');
+  
+  increaseNewchImage.addEventListener("mouseover",function(){
+    newch.classList.add("active");
+  })
+  
+  increaseNewchmage.addEventListener("mouseout",function(){
+      newch.classList.remove("active");
+  })
 
-base("books").select({}).eachPage(gotPageOfBooks, gotAllBooks);
+  
 
-//an empty array to hold our book data
-const books = [];
+  //second image
+ 
+  let memo = document.querySelector(".memo");
 
-//callback function that receives our data
-function gotPageOfBooks(records, fetchNextPage) {
-    console.log("gotPageOfBooks()");
-    // add the records from this page to our books array
-    books.push(...records);
-    // request more pages
-    fetchNextPage();
-}
-
-// call back function that is called when all pages a 
-function gotAllBooks(err) {
-    console.log("gotAllBooks()");
-
-// report an error, tou'd want to do something better
-if (err) {
-    console.log("error loading books");
-    console.error(err);
-    return;
-}
-
-// call functions to log and show the books 
-consoleLogBooks();
-showBooks();
-}
-
-
-// just loop throught the books and console.
-function consoleLogBooks() {
-    console.log("consoleLogBooks()");
-    books.forEach((book) => {
-        console.log("Book:", book);
-    });
-}
-
-function showBooks() {
-    console.log("showBooks()");
-    books.forEach((book) => {
-        const h2 = document.createElement("h2");
-        h2.innerText = book.fields.title;
-        document.body.appendChild(h2);
-    });
-}
+  //the word halfway
+  let increaseMemoImage = document.querySelector('.second-poster');
+  
+  increaseMemoImage.addEventListener("mouseover",function(){
+    memo.classList.add("active");
+  })
+  
+  increaseMemoImage.addEventListener("mouseout",function(){
+      memo.classList.remove("active");
+  })
