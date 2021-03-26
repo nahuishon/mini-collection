20fdaa13cb7ea501932c5b0c5c43eba912a87165
nahuishon <shonn620@newschool.edu>
2021-03-26 21:22:15 +0900

    S

--- main.js
+++ main.js
@@ -9,17 +9,17 @@ document.getElementById("demo").innerHTML = "My Favorite Korean Movie";
 //FIRST IMFORMATION
 
 document.getElementById("button").addEventListener("click", function() {
-    document.getElementById("detail").innerHTML = "PARASITE / BONG JOON-HO";
+    document.getElementById("detail").innerHTML = "PARASITE / BONG JOON-HO / The struggling Kim family sees an opportunity when the son starts working for the wealthy Park family. Soon, all of them find a way to work within the same household and start living a parasitic life.";
   })
 
   //SECOND IMFORMATION
-  document.getElementById("button").addEventListener("click", function() {
-    document.getElementById("detail").innerHTML = "PARASITE / BONG JOON-HO";
+  document.getElementById("button2").addEventListener("click", function() {
+    document.getElementById("detail2").innerHTML = "Memories of Murder/ BONG JOON-HO / A big-city detective helps two inept small-town cops investigate a serial killer.";
   })
 
   //LAST IMFORMATION
-  document.getElementById("button").addEventListener("click", function() {
-    document.getElementById("detail").innerHTML = "PARASITE / BONG JOON-HO";
+  document.getElementById("button3").addEventListener("click", function() {
+    document.getElementById("detail3").innerHTML = "Ode to My Father / BONG JOON-HO / Separated during wartime in North Korea, a man desperately longs to reunite with his missing father and sister.";
   })
 
   //SECNOND IMAGE START 
